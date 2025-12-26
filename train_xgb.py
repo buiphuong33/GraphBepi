d073108bedf8fdd53a64155506cb9675980a31fe
Bui Phuong <buithuphuongg1303@gmail.com>
2025-12-27 00:54:06 +0700

    debug

--- train_xgb.py
+++ train_xgb.py
@@ -62,7 +62,7 @@ def main(args):
         objective='binary:logistic',
         eval_metric='aucpr',
         use_label_encoder=False,
-        n_estimators=1000,
+        n_estimators=20,
         learning_rate=0.05,
         max_depth=6,
         subsample=0.8,
